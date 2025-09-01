51e8072a65cbb895c75d997284f24b618cca6964
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-09-01 13:02:45 +0530

    add: master component for pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -5,6 +5,7 @@ import Empty from "../../ui/Empty";
 import Spinner from "../../ui/Spinner";
 
 import { useBookings } from "./useBookings";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   // const bookings = [];
@@ -12,11 +13,11 @@ function BookingTable() {
 
   if (isLoading) return <Spinner />;
 
-  if (!bookings.length) return <Empty resourceName='bookings' />;
+  if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
     <Menus>
-      <Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
+      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
         <Table.Header>
           <div>Cabin</div>
           <div>Guest</div>
@@ -32,6 +33,10 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
