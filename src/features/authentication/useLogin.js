f88913b6f2ec9d820a06023e0b14b3e35d254d6a
AMRIT <amd@Amrits-MacBook-Air.local>
2025-09-09 17:09:58 +0530

    auth: protecting the routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isPending: isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: () => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
 
